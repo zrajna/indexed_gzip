6788887ed756edf839c4659137564fcdf9d706b1
Paul McCarthy <pauld.mccarthy@gmail.com>
2016-02-25 21:20:17 +0100

    New function zran_read which reads and returns a block of uncompressed data (assuming a prior call to zran_seek). Changed naming convention 'zran_index_' to 'zran_'. Fixed a couple of bugs, and introduced a bug with insanely verbose logging, which took ages to track down and fix. Ridiculous logging will be removed soon

--- zran.c
+++ zran.c
@@ -5,7 +5,7 @@
 
 
 #ifdef ZRAN_VERBOSE
-#define zran_log(...) printf(__VA_ARGS__)
+#define zran_log(...) fprintf(stderr, __VA_ARGS__)
 #else
 #define zran_log(...) 
 #endif
@@ -49,12 +49,13 @@ struct _zran_index {
     int           have; /* number of list entries filled in */
     int           size; /* number of list entries allocated */
     zran_point_t *list; /* allocated list */
+    off_t         uncmp_seek_offset;
 };
 
 
-static void zran_index_new(zran_index_t *index) {
+static void zran_new(zran_index_t *index) {
 
-    zran_log("zran_index_new\n");
+    zran_log("zran_new\n");
 
     index->span = 0;
     index->have = 0;
@@ -62,9 +63,9 @@ static void zran_index_new(zran_index_t *index) {
     index->list = NULL;
 };
 
-static int zran_index_init(zran_index_t *index, int span) {
+static int zran_init(zran_index_t *index, int span) {
 
-    zran_log("zran_index_init (%i)\n", span);
+    zran_log("zran_init (%i)\n", span);
 
     if (index->list != NULL)
         return -1;
@@ -84,11 +85,11 @@ static int zran_index_init(zran_index_t *index, int span) {
 };
 
 
-static int zran_index_expand(zran_index_t *index) {
+static int zran_expand(zran_index_t *index) {
 
     int new_size = index->size * 2;
 
-    zran_log("zran_index_expand (%i -> %i)\n", index->size, new_size);
+    zran_log("zran_expand (%i -> %i)\n", index->size, new_size);
     
     zran_point_t *new_list = realloc(index->list,
                                      sizeof(zran_point_t) * new_size);
@@ -104,9 +105,9 @@ static int zran_index_expand(zran_index_t *index) {
 };
 
 
-static int zran_index_free_unused(zran_index_t *index) {
+static int zran_free_unused(zran_index_t *index) {
 
-    zran_log("zran_index_free_unused\n");
+    zran_log("zran_free_unused\n");
 
     zran_point_t *new_list;
 
@@ -124,9 +125,9 @@ static int zran_index_free_unused(zran_index_t *index) {
 
 
 /* Deallocate an index built by build_index() */
-static void zran_index_dealloc(zran_index_t *index) {
+static void zran_dealloc(zran_index_t *index) {
 
-    zran_log("zran_index_dealloc\n");
+    zran_log("zran_dealloc\n");
     
     if (index->list != NULL) {
         free(index->list);
@@ -139,12 +140,13 @@ static void zran_index_dealloc(zran_index_t *index) {
 };
 
 
-static zran_point_t * zran_index_get_point_at(zran_index_t *index,
-                                              off_t         offset,
-                                              char          compressed) {
+static zran_point_t * zran_get_point_at(zran_index_t *index,
+                                        off_t         offset,
+                                        char          compressed) {
 
     zran_point_t *prev;
     zran_point_t *curr;
+    int           bit;
     int           i;
 
     prev = index->list;
@@ -153,9 +155,13 @@ static zran_point_t * zran_index_get_point_at(zran_index_t *index,
     for (i = 1; i < index->size; i++) {
         
         curr = &(index->list[i]);
-        
+
         if (compressed) {
-            if (curr->cmp_offset > offset) 
+
+            if (curr->bits > 0) bit = 1;
+            else                bit = 0;
+            
+            if (curr->cmp_offset > offset + bit) 
                 return prev;
                 
         }
@@ -163,6 +169,8 @@ static zran_point_t * zran_index_get_point_at(zran_index_t *index,
             if (curr->uncmp_offset > offset) 
                 return prev;
         }
+
+        prev = curr;
     }
 
     return NULL;
@@ -177,13 +185,13 @@ static int zran_add_point(zran_index_t  *index,
                           unsigned       left,
                           unsigned char *window) {
 
-    zran_log("zran_index_add_point(%lld <-> %lld)\n", cmp_offset, uncmp_offset);
+    zran_log("zran_add_point(%i, %lld <-> %lld)\n", index->have, cmp_offset, uncmp_offset);
 
     zran_point_t *next;
 
     /* if list is full, make it bigger */
     if (index->have == index->size) {
-        if (zran_index_expand(index) != 0) {
+        if (zran_expand(index) != 0) {
             return -1;
         }
     }
@@ -296,14 +304,13 @@ static int zran_build_full_index(zran_index_t *index, FILE *in) {
         } while (strm.avail_in != 0);
     } while (ret != Z_STREAM_END);
 
-    /* clean up and return index (release unused entries in list) */
-    (void)inflateEnd(&strm);
-
-    if (zran_index_free_unused(index) != 0) {
+    if (zran_free_unused(index) != 0) {
         ret = Z_MEM_ERROR;
         goto build_index_error;
     }
-
+    
+    /* clean up and return index (release unused entries in list) */
+    inflateEnd(&strm);
     return index->size;
 
     /* return error */
@@ -333,13 +340,14 @@ static int zran_seek(zran_index_t  *index,
         return -1;
     }
 
-    seek_point = zran_index_get_point_at(index, offset, 0);
+    seek_point = zran_get_point_at(index, offset, 0);
 
     if (seek_point == NULL) {
         return -1;
     }
 
-    offset = seek_point->cmp_offset;
+    index->uncmp_seek_offset = offset;
+    offset                   = seek_point->cmp_offset;
 
     if (seek_point->bits > 0)
         offset -= 1;
@@ -349,6 +357,169 @@ static int zran_seek(zran_index_t  *index,
     *point = seek_point;
 
     return fseeko(in, offset, SEEK_SET);
+}
+
+
+static int zran_read(zran_index_t  *index,
+                     FILE          *in,
+                     unsigned char *buf,
+                     int            len) {
+
+    int           ch;
+    off_t         uncmp_offset;
+    off_t         cmp_offset;
+    int           skip;
+    z_stream      strm;
+    zran_point_t *point;
+    unsigned char input[CHUNK];
+    unsigned char discard[WINSIZE];
+
+    memset(input,   0, CHUNK);
+    memset(discard, 0, WINSIZE); 
+
+    zran_log("zran_read(%i)\n", len);
+
+    // silly input
+    if (len < 0)
+        return 0;
+
+    // Get the current location
+    // in the compressed stream.
+    cmp_offset   = ftello(in);
+    uncmp_offset = index->uncmp_seek_offset;
+             
+    zran_log("Offsets: compressed=%lld, uncompressed=%lld\n",
+             cmp_offset,
+             uncmp_offset);
+
+    if (cmp_offset < 0) 
+        return -1;
+
+    // Get the current index point
+    // that corresponds to this
+    // location.
+    point = zran_get_point_at(index, cmp_offset, 1);
+
+    if (point == NULL) 
+        return -1;
+
+    zran_log("Identified access point: %lld - %lld\n",
+             point->cmp_offset,
+             point->uncmp_offset);
+    
+    /* initialize file and inflate state to start there */
+    strm.zalloc   = Z_NULL;
+    strm.zfree    = Z_NULL;
+    strm.opaque   = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in  = Z_NULL;
+    
+    if (inflateInit2(&strm, -15) != Z_OK)
+        goto fail;
+
+    // The compressed location is
+    // not byte-aligned with the
+    // uncompressed location.
+    if (point->bits) {
+        ch = getc(in);
+        if (ch == -1) 
+            goto fail;
+
+        zran_log("inflatePrime(%i, %i)\n",
+                 point->bits,
+                 ch >> (8 - point->bits)); 
+
+        if (inflatePrime(&strm, point->bits, ch >> (8 - point->bits)) != Z_OK)
+            goto fail;
+    }
+
+    zran_log("InflateSetDictionary( %i %i %i ...)\n",
+             point->window[0],
+             point->window[1],
+             point->window[2]);
+    if (inflateSetDictionary(&strm, point->window, WINSIZE) != Z_OK)
+        goto fail;
+
+    /* skip uncompressed bytes until offset reached, then satisfy request */
+    zran_log("Initial offset: %lld - %lld\n", uncmp_offset, point->uncmp_offset);
+
+    uncmp_offset -= point->uncmp_offset;
+    
+    strm.avail_in = 0;
+    skip = 1; /* while skipping to offset */
+    do {
+        /* define where to put uncompressed data, and how much */
+        if (uncmp_offset == 0 && skip) {          /* at offset now */
+            strm.avail_out = len;
+            strm.next_out = buf;
+            skip = 0;                       /* only do this once */
+        }
+        if (uncmp_offset > WINSIZE) {             /* skip WINSIZE bytes */
+            strm.avail_out = WINSIZE;
+            strm.next_out = discard;
+            uncmp_offset -= WINSIZE;
+        }
+        else if (uncmp_offset != 0) {             /* last skip */
+            strm.avail_out = (unsigned)uncmp_offset;
+            strm.next_out = discard;
+            uncmp_offset = 0;
+        }
+
+        /* uncompress until avail_out filled, or end of stream */
+        do {
+            if (strm.avail_in == 0) {
+                strm.avail_in = fread(input, 1, CHUNK, in);
+                if (ferror(in)) {
+                    goto fail;
+                }
+                if (strm.avail_in == 0) {
+                    goto fail;
+                }
+                strm.next_in = input;
+            }
+            /* normal inflate */
+            zran_log("Call inflate\n");
+            zran_log("  Stream status:\n");
+            zran_log("    avail_in:  %i\n", strm.avail_in);
+            zran_log("    avail_out: %i\n", strm.avail_out);
+            zran_log("    next_in:   %u\n", strm.next_in[0]);
+            zran_log("    next_out:  %u\n", strm.next_out[0]);
+            zran_log("    input:     %u\n", input[0]);
+            zran_log("    buf:       %u\n", buf[0]);
+            zran_log("    discard:   %u\n", discard[0]);
+            
+            ch = inflate(&strm, Z_NO_FLUSH);
+            zran_log("Inflate called\n");
+
+            if (ch == Z_STREAM_END)
+                break;
+            else if (ch != Z_OK) {
+                zran_log("Return code not ok: %i\n", ch);
+                goto fail;
+            }
+
+        } while (strm.avail_out != 0);
+
+        /* if reach end of stream, then don't keep trying to get more */
+        if (ch == Z_STREAM_END)
+            break;
+
+        /* do until offset reached and requested data read, or stream ends */
+    } while (skip);
+
+    /* compute number of uncompressed bytes read after offset */
+    zran_log("Call inflateEnd\n");
+    inflateEnd(&strm);
+    zran_log("inflateEnd called\n");
+    
+    return skip ? 0 : len - strm.avail_out;
+
+    /* clean up and return bytes read or error */
+fail:
+    zran_log("(fail) Call inflateEnd\n");
+    inflateEnd(&strm);
+    zran_log("(fail) inflateEnd called\n");
+    return -1; 
 }
 
 
@@ -369,91 +540,12 @@ static int zran_extract(zran_index_t *index,
     if (len < 0)
         return 0;
 
+    zran_log("Starting seek ... \n");
     if (zran_seek(index, in, offset, SEEK_SET, &here) != 0)
         return -1;
 
-    /* initialize file and inflate state to start there */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    ret = inflateInit2(&strm, -15);         /* raw inflate */
-    
-    if (ret != Z_OK)
-        return ret;
-
-    if (ret == -1)
-        goto extract_ret;
-    if (here->bits) {
-        ret = getc(in);
-        if (ret == -1) {
-            ret = ferror(in) ? Z_ERRNO : Z_DATA_ERROR;
-            goto extract_ret;
-        }
-        (void)inflatePrime(&strm, here->bits, ret >> (8 - here->bits));
-    }
-    (void)inflateSetDictionary(&strm, here->window, WINSIZE);
-
-    /* skip uncompressed bytes until offset reached, then satisfy request */
-    offset -= here->uncmp_offset;
-    strm.avail_in = 0;
-    skip = 1;                               /* while skipping to offset */
-    do {
-        /* define where to put uncompressed data, and how much */
-        if (offset == 0 && skip) {          /* at offset now */
-            strm.avail_out = len;
-            strm.next_out = buf;
-            skip = 0;                       /* only do this once */
-        }
-        if (offset > WINSIZE) {             /* skip WINSIZE bytes */
-            strm.avail_out = WINSIZE;
-            strm.next_out = discard;
-            offset -= WINSIZE;
-        }
-        else if (offset != 0) {             /* last skip */
-            strm.avail_out = (unsigned)offset;
-            strm.next_out = discard;
-            offset = 0;
-        }
-
-        /* uncompress until avail_out filled, or end of stream */
-        do {
-            if (strm.avail_in == 0) {
-                strm.avail_in = fread(input, 1, CHUNK, in);
-                if (ferror(in)) {
-                    ret = Z_ERRNO;
-                    goto extract_ret;
-                }
-                if (strm.avail_in == 0) {
-                    ret = Z_DATA_ERROR;
-                    goto extract_ret;
-                }
-                strm.next_in = input;
-            }
-            ret = inflate(&strm, Z_NO_FLUSH);       /* normal inflate */
-            if (ret == Z_NEED_DICT)
-                ret = Z_DATA_ERROR;
-            if (ret == Z_MEM_ERROR || ret == Z_DATA_ERROR)
-                goto extract_ret;
-            if (ret == Z_STREAM_END)
-                break;
-        } while (strm.avail_out != 0);
-
-        /* if reach end of stream, then don't keep trying to get more */
-        if (ret == Z_STREAM_END)
-            break;
-
-        /* do until offset reached and requested data read, or stream ends */
-    } while (skip);
-
-    /* compute number of uncompressed bytes read after offset */
-    ret = skip ? 0 : len - strm.avail_out;
-
-    /* clean up and return bytes read or error */
-  extract_ret:
-    (void)inflateEnd(&strm);
-    return ret;
+    zran_log("Starting read ... \n");
+    return zran_read(index, in, buf, len);
 };
 
 
@@ -482,8 +574,8 @@ int main(int argc, char **argv)
     }
 
     /*Initialise the index*/
-    zran_index_new( &index);
-    zran_index_init(&index, 1048576);
+    zran_new( &index);
+    zran_init(&index, 1048576);
 
     /* build index */
     len = zran_build_full_index(&index, in);
@@ -519,7 +611,7 @@ int main(int argc, char **argv)
     }
 
     /* clean up and exit */
-    zran_index_dealloc(&index);
+    zran_dealloc(&index);
     fclose(in);
     return 0;
 };
@@ -558,7 +650,7 @@ static PyObject * zran_ZIndex_new(PyTypeObject *type,
     if (self == NULL) 
         goto fail;
 
-    zran_index_new(&(self->index));
+    zran_new(&(self->index));
 
     self->fid            = NULL;
     self->span           = 0;
@@ -609,7 +701,7 @@ static int zran_ZIndex_init(ZIndex   *self,
 
     fid = fdopen(PyObject_AsFileDescriptor(py_fid), "rb");
 
-    zran_index_init(&(self->index), span);
+    zran_init(&(self->index), span);
 
     self->py_fid         = py_fid;
     self->fid            = fid;
@@ -630,7 +722,7 @@ fail:
 
 static void zran_ZIndex_dealloc(ZIndex *self) {
     
-    zran_index_dealloc(&(self->index));
+    zran_dealloc(&(self->index));
 
     self->fid            = NULL;
     self->span           = 0;

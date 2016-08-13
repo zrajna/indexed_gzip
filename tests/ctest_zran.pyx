d2c6df43dd83ea825fa6161db1af2f5fa17af604
Paul McCarthy <pauld.mccarthy@gmail.com>
2016-08-13 13:36:55 +0100

    Added some test cases

--- tests/ctest_zran.pyx
+++ tests/ctest_zran.pyx
@@ -17,17 +17,22 @@ import              hashlib
 import numpy     as np
 
 
-from libc.stdio cimport (FILE, fdopen)
+from libc.stdio cimport (SEEK_SET, FILE, fdopen)
+
+from cpython.mem cimport (PyMem_Malloc,
+                          PyMem_Realloc,
+                          PyMem_Free)
 
 cimport zran
+
 
 # 2**32 values, at 2 bytes each, is 8GB
 # 2**31 values, at 2 bytes each, is 4GB
 # 2**30 values, at 2 bytes each, is 2GB
 # 2**29 values, at 2 bytes each, is 1GB
-TEST_FILE_SIZE = 2**29
-TEST_FILE      = 'testdata.gz'
-
+TEST_FILE_NELEMS = 2**29
+TEST_FILE_SIZE   = TEST_FILE_NELEMS * 2
+TEST_FILE        = 'testdata.gz'
 
 
 def setup_module():
@@ -45,7 +50,62 @@ def setup_module():
 def teardown_module():
     pass
 
+
+
+
+def gen_test_data(filename):
+    """Make some data to test with. """
+    
+    print('Generating test data')
+
+    data = np.random.randint(0, 65535, TEST_FILE_NELEMS)
+    data = np.array(data, dtype=np.uint16)
+                  
+    with gzip.GzipFile(filename, 'wb') as f:
+        f.write(data.tostring())
+
+
+def md5(data):
+    
+    hashobj = hashlib.md5()
+    hashobj.update(data)
+    return str(hashobj.hexdigest())
+
+
+cdef class ReadBuffer:
+    """Wrapper around a chunk of memory.
+ 
+    .. see:: http://docs.cython.org/src/tutorial/memory_allocation.html
+    """
+
+    cdef void *buffer
+    """A raw chunk of bytes. """
+
+    
+    def __cinit__(self, size_t size):
+        """Allocate ``size`` bytes of memory. """
+
+        self.buffer = PyMem_Malloc(size);
+
+        if not self.buffer:
+            raise MemoryError('PyMem_Malloc fail')
+
+
+    def resize(self, size_t size):
+        """Re-allocate the memory to the given ``size``. """
         
+        buf = PyMem_Realloc(self.buffer, size)
+
+        if not buf:
+            raise MemoryError('PyMem_Realloc fail')
+
+        self.buffer = buf
+
+
+    def __dealloc__(self):
+        """Free the mwmory. """
+        PyMem_Free(self.buffer)
+
     
 def test_init():
     """Tests a bunch of permutations of the parameters to zran_init. """
@@ -85,6 +145,8 @@ def test_init():
                       s, w, r, f, expected, result))
             
             assert result == expected
+
+            zran.zran_free(&index)
                 
 
 
@@ -92,14 +154,17 @@ def test_init_file_modes():
 
     modes = ['r', 'r+', 'w', 'w+', 'a', 'a+']
 
+    files = [TEST_FILE, TEST_FILE,
+             'dummy.gz', 'dummy.gz', 'dummy.gz', 'dummy.gz']
+
     cdef zran.zran_index_t index
     cdef FILE             *cfid
     cdef bytes             bmode
     cdef char             *cmode
 
-    for mode in modes:
+    for filename, mode in zip(files, modes):
 
-        with open(TEST_FILE, mode) as pyfid:
+        with open(filename, mode) as pyfid:
 
             bmode    = mode.encode()
             cmode    = bmode
@@ -114,31 +179,121 @@ def test_init_file_modes():
 
             assert result == expected
 
+            zran.zran_free(&index)
+
 
 def test_seek_and_tell():
 
-    seekpoints = [random.randint(0, TEST_FILE_SIZE) for i in range(100)]
+    cdef zran.zran_index_t index
+
+    seekpoints   = [random.randint(0, TEST_FILE_SIZE) for i in range(500)]
+    indexSpacing = TEST_FILE_SIZE / 1000
 
     with open(TEST_FILE, 'rb') as pyfid:
         cfid = fdopen(pyfid.fileno(), 'rb')
 
-        # TODO ...
+        assert not zran.zran_init(&index,
+                                  cfid,
+                                  indexSpacing,
+                                  32768,
+                                  131072,
+                                  zran.ZRAN_AUTO_BUILD)
+
+        for sp in seekpoints:
+            
+            zran.zran_seek(&index, sp, SEEK_SET, NULL)
+            zt = zran.zran_tell(&index)
+            print('{} == {}?'.format(zt, sp))
+            assert zt == sp
+
+        zran.zran_free(&index)
 
 
-def gen_test_data(filename):
-    """Make some data to test with. """
+def test_read_all():
+
+    indexSpacing = TEST_FILE_SIZE / 1000
     
-    print('Generating test data')
+    cdef zran.zran_index_t index
+    cdef void             *buffer
 
-    data = np.random.randint(0, 65535, TEST_FILE_SIZE)
-    data = np.array(data, dtype=np.uint16)
-                  
-    with gzip.GzipFile(filename, 'wb') as f:
-        f.write(data.tostring())
+    with open(TEST_FILE, 'rb') as pyfid:
+        cfid = fdopen(pyfid.fileno(), 'rb')
+
+        buf = ReadBuffer(TEST_FILE_SIZE)
+
+        buffer = buf.buffer
+
+        assert not zran.zran_init(&index,
+                                  cfid,
+                                  indexSpacing,
+                                  32768,
+                                  131072,
+                                  zran.ZRAN_AUTO_BUILD)
+
+        nbytes = zran.zran_read(&index, buffer, TEST_FILE_SIZE)
+
+        assert nbytes == TEST_FILE_SIZE
+        
+        zran.zran_free(&index)
 
 
-def md5(data):
+def test_seek_then_read_all():
     
-    hashobj = hashlib.md5()
-    hashobj.update(data)
-    return str(hashobj.hexdigest())
+    indexSpacing = TEST_FILE_SIZE / 1000
+    buf          = ReadBuffer(TEST_FILE_SIZE)
+    seekpoints   = [random.randint(0, TEST_FILE_SIZE) for i in range(500)]
+    
+    cdef zran.zran_index_t index
+    cdef void             *buffer = buf.buffer
+
+    with open(TEST_FILE, 'rb') as pyfid:
+        cfid = fdopen(pyfid.fileno(), 'rb')
+
+        assert not zran.zran_init(&index,
+                                  cfid,
+                                  indexSpacing,
+                                  32768,
+                                  131072,
+                                  zran.ZRAN_AUTO_BUILD)
+
+        for sp in seekpoints:
+
+            zran.zran_seek(&index, sp, SEEK_SET, NULL)
+
+            nbytes = zran.zran_read(&index, buffer, TEST_FILE_SIZE)
+
+            assert nbytes == TEST_FILE_SIZE - sp
+        
+        zran.zran_free(&index) 
+
+
+def test_build_then_read():
+    
+    indexSpacing = TEST_FILE_SIZE / 1000
+    buf          = ReadBuffer(TEST_FILE_SIZE)
+    seekpoints   = [random.randint(0, TEST_FILE_SIZE) for i in range(500)]
+    
+    cdef zran.zran_index_t index
+    cdef void             *buffer = buf.buffer
+
+    with open(TEST_FILE, 'rb') as pyfid:
+        cfid = fdopen(pyfid.fileno(), 'rb')
+
+        assert not zran.zran_init(&index,
+                                  cfid,
+                                  indexSpacing,
+                                  32768,
+                                  131072,
+                                  zran.ZRAN_AUTO_BUILD)
+
+        assert not zran.zran_build_index(&index, 0, 0)
+
+        for sp in seekpoints:
+
+            zran.zran_seek(&index, sp, SEEK_SET, NULL)
+
+            nbytes = zran.zran_read(&index, buffer, TEST_FILE_SIZE)
+
+            assert nbytes == TEST_FILE_SIZE - sp
+        
+        zran.zran_free(&index) 

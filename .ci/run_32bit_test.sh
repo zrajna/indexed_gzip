9e2e8aa0c3269eaf8e03bd7c9dd8f9747805b312
Paul McCarthy <pauldmccarthy@gmail.com>
2017-09-10 12:09:29 +0100

    Experimental test for 32 bit platforms. Not sure if this will work

---
+++ .ci/run_32bit_test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This script is called via .travis.yml. It is not intended
+# to be called in any other manner.
+#
+
+apt-get update
+apt-get install -y libssl-dev openssl wget build-essential
+cd /
+wget https://www.python.org/ftp/python/3.5.3/Python-3.5.3.tgz
+tar xf Python-3.5.3.tgz
+cd Python-3.5.3
+./configure --prefix=/python35
+make && make install
+cd /python35/bin
+ln -s python3.5 python
+ln -s pip3 pip
+
+export PATH=/python35/bin:$PATH
+
+pip install numpy pytest cython
+
+cd $INDEXED_GZIP_DIR
+python setup.py develop
+python setup.py test --addopts "-v -s --niters 500";
+python setup.py test --addopts "-v -s --niters 500 --concat";
---
+++ .ci/run_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# This script is called via .travis.yml. It is not intended
+# to be called in any other manner.
+#
+
+# Get the path to this script
+script_dir=`dirname $0`
+pushd $script_dir > /dev/null
+script_dir=`pwd`
+pushd .. > /dev/null
+igzipdir=`pwd`
+popd     > /dev/null
+popd     > /dev/null
+
+# 32 bit platform test has to be run in a docker container
+if [ "$TEST_SUITE" == "32bittest" ]; then
+    docker run --rm \
+           -e INDEXED_GZIP_DIR="$igzip_dir" \
+           -v $igzip_dir:/indexed_gzip \
+           32bit/ubuntu:16.04 \
+           /indexed_gzip/.ci/run_32bit_test.sh
+
+# Run standard test suite
+else
+    python setup.py develop;
+    python setup.py test --addopts "-v -s -m \"$TEST_SUITE\" -k \"$TEST_PATTERN\" --nelems \"$NELEMS\" --niters \"$NITERS\" $EXTRA_ARGS";
+fi

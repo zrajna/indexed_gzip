beaef0c61eb4835846f316f51a73285f63d0f3e0
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-09-21 11:15:54 +0100

    First pass at Windows build

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import sys
 import os
 import glob
 import os.path as op
@@ -62,36 +63,57 @@ class Clean(Command):
 # We only need numpy to
 # compile the test modules
 have_cython = True
-have_numpy  = True
+build_tests  = True
 
+# Platform information
+python2 = (sys.version_info[0] == 2)
+windows = sys.platform.startswith("win")
 
 try:    from Cython.Build import cythonize
 except: have_cython = False
 
 try:    import numpy as np
-except: have_numpy = False
+except: build_tests = False
 
+include_dirs = ['indexed_gzip']
+lib_dirs = []
+libs = []
+extra_compile_args = []
 
 # If numpy is present, we need
 # to include the headers
-include_dirs = ['indexed_gzip']
-if have_numpy:
+if build_tests:
     include_dirs.append(np.get_include())
 
+if windows:
+    build_tests = False
+    ZLIB_HOME = os.environ.get("FSLDIR", "c:/Program Files (x86)/GnuWin32")
+    include_dirs.append(os.path.join(ZLIB_HOME, "include"))
+    libs.append('zlib')
+    lib_dirs.append(os.path.join(ZLIB_HOME, "lib"))
+    if python2:
+        # For stdint.h which is not included in the old Visual C 
+        # compiler used for Python 2
+        include_dirs.append('compat')
+        extra_compile_args += ['-DNO_C99']
+else:
+    libs.append('z')
+    extra_compile_args += ['-Wall', '-pedantic', '-Wno-unused-function']
 
 # Compile from cython files if
 # possible, or compile from c.
 if have_cython: pyx_ext = 'pyx'
 else:           pyx_ext = 'c'
 
-
 # The indexed_gzip module
 igzip_ext = Extension(
     'indexed_gzip.indexed_gzip',
     [op.join('indexed_gzip', 'indexed_gzip.{}'.format(pyx_ext)),
      op.join('indexed_gzip', 'zran.c')],
-    libraries=['z'],
-    extra_compile_args=['-Wall', '-pedantic', '-Wno-unused-function'])
+    libraries=libs,
+    library_dirs=lib_dirs,
+    include_dirs=include_dirs,
+    extra_compile_args=extra_compile_args)
 
 # Optional test modules
 test_exts = [
@@ -99,20 +121,24 @@ test_exts = [
         'indexed_gzip.tests.ctest_zran',
         [op.join('indexed_gzip', 'tests', 'ctest_zran.{}'.format(pyx_ext)),
          op.join('indexed_gzip', 'zran.c')],
-        libraries=['z'],
+        libraries=libs,
+        library_dirs=lib_dirs,
         include_dirs=include_dirs,
-        extra_compile_args=['-Wno-unused-function']),
+        extra_compile_args=extra_compile_args),
+#        extra_compile_args=['-Wno-unused-function']),
     Extension(
         'indexed_gzip.tests.ctest_indexed_gzip',
         [op.join('indexed_gzip', 'tests',
                  'ctest_indexed_gzip.{}'.format(pyx_ext))],
-        libraries=['z'],
+        libraries=libs,
+        library_dirs=lib_dirs,
         include_dirs=include_dirs,
-        extra_compile_args=['-Wno-unused-function'])]
+        extra_compile_args=extra_compile_args)]
+#        extra_compile_args=['-Wno-unused-function'])]
 
 
 # If we have numpy, we can compile the tests
-if have_numpy: extensions = [igzip_ext] + test_exts
+if build_tests: extensions = [igzip_ext] + test_exts
 else:          extensions = [igzip_ext]
 
 

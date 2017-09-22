7d54a9d51ee035e9658f33525cf101cc41950d50
Martin Craig <martin.craig@eng.ox.ac.uk>
2017-09-22 11:51:27 +0100

    Removed tests which don't run under Windows

--- indexed_gzip/tests/test_safe_indexed_gzip.py
+++ indexed_gzip/tests/test_safe_indexed_gzip.py
@@ -8,58 +8,61 @@
 from __future__ import print_function
 from __future__ import division
 
-import threading
+import sys
 
-import numpy as np
+if not sys.platform.startswith("win"):
+    import threading
 
-import pytest
+    import numpy as np
 
-import indexed_gzip as igzip
+    import pytest
 
-from . import test_indexed_gzip as test_igzip
-from . import ctest_zran
+    import indexed_gzip as igzip
 
-
-pytestmark = pytest.mark.indexed_gzip_test
-
-
-def test_SafeIndexedGzipFile_open_close(testfile):
-
-    f = igzip.SafeIndexedGzipFile(filename=testfile)
-    f.seek(10)
-    f.read(10)
-    f.close()
+    from . import test_indexed_gzip as test_igzip
+    from . import ctest_zran
 
 
-def test_SafeIndexedGzipFile_pread_threaded(testfile, nelems):
+    pytestmark = pytest.mark.indexed_gzip_test
 
-    filesize     = nelems * 8
-    indexSpacing = max(524288, filesize // 2000)
 
-    with igzip.SafeIndexedGzipFile(filename=testfile,
-                                   spacing=indexSpacing) as f:
+    def test_SafeIndexedGzipFile_open_close(testfile):
 
-        readelems = 50
-        readsize  = readelems * 8
-        nthreads  = 100
-        allreads  = []
+        f = igzip.SafeIndexedGzipFile(filename=testfile)
+        f.seek(10)
+        f.read(10)
+        f.close()
 
-        def do_pread(nbytes, offset):
-            data = f.pread(nbytes, offset * 8)
-            allreads.append((offset, data))
 
-        offsets = np.linspace(0, nelems - readelems, nthreads, dtype=np.uint64)
-        threads = [threading.Thread(target=do_pread, args=(readsize, o))
-                   for o in offsets]
-        [t.start() for t in threads]
-        [t.join()  for t in threads]
+    def test_SafeIndexedGzipFile_pread_threaded(testfile, nelems):
 
-        assert len(allreads) == nthreads
-        for offset, data in allreads:
+        filesize     = nelems * 8
+        indexSpacing = max(524288, filesize // 2000)
 
-            assert len(data) == readsize
+        with igzip.SafeIndexedGzipFile(filename=testfile,
+                                    spacing=indexSpacing) as f:
 
-            data = np.ndarray(shape=readelems, dtype=np.uint64, buffer=data)
-            assert ctest_zran.check_data_valid(data,
-                                               offset,
-                                               offset + readelems)
+            readelems = 50
+            readsize  = readelems * 8
+            nthreads  = 100
+            allreads  = []
+
+            def do_pread(nbytes, offset):
+                data = f.pread(nbytes, offset * 8)
+                allreads.append((offset, data))
+
+            offsets = np.linspace(0, nelems - readelems, nthreads, dtype=np.uint64)
+            threads = [threading.Thread(target=do_pread, args=(readsize, o))
+                    for o in offsets]
+            [t.start() for t in threads]
+            [t.join()  for t in threads]
+
+            assert len(allreads) == nthreads
+            for offset, data in allreads:
+
+                assert len(data) == readsize
+
+                data = np.ndarray(shape=readelems, dtype=np.uint64, buffer=data)
+                assert ctest_zran.check_data_valid(data,
+                                                offset,
+                                                offset + readelems)

d5d8fbd69507cc196a3cee95742de00876f67326
Paul McCarthy <pauldmccarthy@gmail.com>
2017-09-23 12:09:05 +0100

    Safe indexed gzip file tests no longer depend on ctest_zran module

--- indexed_gzip/tests/test_safe_indexed_gzip.py
+++ indexed_gzip/tests/test_safe_indexed_gzip.py
@@ -10,59 +10,56 @@ from __future__ import division
 
 import sys
 
-if not sys.platform.startswith("win"):
-    import threading
+import threading
 
-    import numpy as np
+import numpy as np
 
-    import pytest
+import pytest
 
-    import indexed_gzip as igzip
+import indexed_gzip as igzip
 
-    from . import test_indexed_gzip as test_igzip
-    from . import ctest_zran
+from . import check_data_valid
 
-
-    pytestmark = pytest.mark.indexed_gzip_test
+pytestmark = pytest.mark.indexed_gzip_test
 
 
-    def test_SafeIndexedGzipFile_open_close(testfile):
+def test_SafeIndexedGzipFile_open_close(testfile):
 
-        f = igzip.SafeIndexedGzipFile(filename=testfile)
-        f.seek(10)
-        f.read(10)
-        f.close()
+    f = igzip.SafeIndexedGzipFile(filename=testfile)
+    f.seek(10)
+    f.read(10)
+    f.close()
 
 
-    def test_SafeIndexedGzipFile_pread_threaded(testfile, nelems):
+def test_SafeIndexedGzipFile_pread_threaded(testfile, nelems):
 
-        filesize     = nelems * 8
-        indexSpacing = max(524288, filesize // 2000)
+    filesize     = nelems * 8
+    indexSpacing = max(524288, filesize // 2000)
 
-        with igzip.SafeIndexedGzipFile(filename=testfile,
-                                    spacing=indexSpacing) as f:
+    with igzip.SafeIndexedGzipFile(filename=testfile,
+                                   spacing=indexSpacing) as f:
 
-            readelems = 50
-            readsize  = readelems * 8
-            nthreads  = 100
-            allreads  = []
+        readelems = 50
+        readsize  = readelems * 8
+        nthreads  = 100
+        allreads  = []
 
-            def do_pread(nbytes, offset):
-                data = f.pread(nbytes, offset * 8)
-                allreads.append((offset, data))
+        def do_pread(nbytes, offset):
+            data = f.pread(nbytes, offset * 8)
+            allreads.append((offset, data))
 
-            offsets = np.linspace(0, nelems - readelems, nthreads, dtype=np.uint64)
-            threads = [threading.Thread(target=do_pread, args=(readsize, o))
-                    for o in offsets]
-            [t.start() for t in threads]
-            [t.join()  for t in threads]
+        offsets = np.linspace(0, nelems - readelems, nthreads,
+                              dtype=np.uint64)
+        threads = [threading.Thread(target=do_pread, args=(readsize, o))
+                   for o in offsets]
+        [t.start() for t in threads]
+        [t.join()  for t in threads]
 
-            assert len(allreads) == nthreads
-            for offset, data in allreads:
+        assert len(allreads) == nthreads
+        for offset, data in allreads:
 
-                assert len(data) == readsize
+            assert len(data) == readsize
 
-                data = np.ndarray(shape=readelems, dtype=np.uint64, buffer=data)
-                assert ctest_zran.check_data_valid(data,
-                                                offset,
-                                                offset + readelems)
+            data = np.ndarray(shape=readelems, dtype=np.uint64,
+                              buffer=data)
+            assert check_data_valid(data, offset, offset + readelems)

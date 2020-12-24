8db8dbe87397d116f8ad56b97b13857b82c72f15
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-24 10:21:09 +0000

    CI: Compile zlib relocatable

--- .ci/build_zlib.sh
+++ .ci/build_zlib.sh
@@ -6,7 +6,9 @@ wget https://www.zlib.net/zlib-1.2.11.tar.gz
 
 tar -xzf zlib-1.2.11.tar.gz
 pushd zlib-1.2.11
-./configure --static
+
+CFLAGS=-fPIC ./configure --static
+
 make
 
 export ZLIB_HOME=$(pwd)

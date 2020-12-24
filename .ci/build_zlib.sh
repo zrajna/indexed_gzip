3fa6816d836d8a18c4aacc94e04d8991a8c4d2b0
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-24 11:48:18 +0000

    CI: This would be so much easier if i had a windows machine

--- .ci/build_zlib.sh
+++ .ci/build_zlib.sh
@@ -7,9 +7,16 @@ curl -o zlib.tar.gz https://www.zlib.net/zlib-1.2.11.tar.gz
 tar -xzf zlib.tar.gz
 pushd zlib-1.2.11
 
-CFLAGS=-fPIC ./configure --static
+if [[ "$PLATFORM" == "windows"* ]]; then
+  cmake -DCMAKE_BUILD_TYPE=Release .
+  cmake --build .
+else
+  CFLAGS=-fPIC ./configure --static
+  make
+fi
 
-make
+ls -l
+
 
 export ZLIB_HOME=$(pwd)
 popd

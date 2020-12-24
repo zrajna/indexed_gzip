646cea4b418078617f29d48e71745768d0c977fa
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-24 18:25:33 +0000

    CI: Change workflows to just download zlib

---
+++ .ci/download_zlib.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Download zlib sources
+#
+set -e
+
+curl -o zlib.tar.gz https://www.zlib.net/zlib-1.2.11.tar.gz
+
+tar -xzf zlib.tar.gz
+
+ZLIB_HOME=$(pwd)/zlib-1.2.11
+
+# used by setup.py
+echo "ZLIB_HOME=$ZLIB_HOME" >> "$GITHUB_ENV"
--- .ci/install_vcpython27.sh
+++ .ci/install_vcpython27.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-
-choco install python2 vcredist2008 cmake
 choco install --ignore-dependencies vcpython27

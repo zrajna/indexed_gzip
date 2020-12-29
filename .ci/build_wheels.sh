fef4dc374650fb348fc426169f461cfa1a51ca0a
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-29 11:33:47 +0000

    CI: Typos

--- .ci/build_wheels.sh
+++ .ci/build_wheels.sh
@@ -2,10 +2,11 @@
 
 python -m pip install cibuildwheel==1.7.2
 
+# Make sure cython is available on all platforms
 export CIBW_BEFORE_BUILD="pip install cython"
 
-# Make sure zlib headers are available
-CIBW_BEFORE_ALL_LINUX="yum install -y zlib-dev"
+# Make sure zlib headers are available on linux
+export CIBW_BEFORE_ALL_LINUX="yum install -y zlib-devel"
 
 # ZLIB is compiled into indexed_gzip on windwos -
 # see .ci/download_zlib.sh and setup.py

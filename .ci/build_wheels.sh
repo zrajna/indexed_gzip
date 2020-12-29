f3fe031bccd34214fff0ae4ec2fab698796c8f61
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-29 20:04:50 +0000

    CI; Windows doesn't like "cmd1; cmd2" - try "cmd1 && cmd2" instead

--- .ci/build_wheels.sh
+++ .ci/build_wheels.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-python -m pip install cibuildwheel==1.7.2
-
 # Make sure cython is available on all platforms
 # Numpy is required to build the test modules
 export CIBW_BEFORE_BUILD="pip install cython numpy"
@@ -13,13 +11,13 @@ export CIBW_BEFORE_ALL_LINUX="yum install -y zlib-devel"
 # see .ci/download_zlib.sh and setup.py
 export CIBW_ENVIRONMENT_WINDOWS="ZLIB_HOME='$ZLIB_HOME'"
 
-export CIBW_BUILD_VERBOSITY=3
-
 # Run quick test suite on built wheels. We need
 # cython for the Cython.Coverage plugin.
 export CIBW_TEST_REQUIRES="cython pytest pytest-cov coverage numpy nibabel"
 PREP="cp {project}/.coveragerc {project}/setup.cfg {project}/conftest.py ."
 RUN="pytest -m 'not slow_test' --pyargs indexed_gzip"
-export CIBW_TEST_COMMAND="$PREP; $RUN"
+export CIBW_TEST_COMMAND="$PREP && $RUN"
+
+python -m pip install cibuildwheel==1.7.2
 
 python -m cibuildwheel --output-dir ./dist
--- .ci/download_zlib.sh
+++ .ci/download_zlib.sh
@@ -11,8 +11,7 @@ tar -xzf zlib.tar.gz
 ZLIB_HOME=$(pwd)/zlib-1.2.11
 
 # if windows, turn /drive/path/to/zlib into
-# drive:/path/to/zlib. Only works for drives
-# with a single letter
+# drive:/path/to/zlib.
 if [[ "$PLATFORM" == "windows"* ]]; then
   drive=$(echo "$ZLIB_HOME" | cut -d / -f 2)
   offset=$(expr ${#drive} + 2)

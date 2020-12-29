0c7beec64e3eeebb0cbb0d8decfd01d955763ab4
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-29 10:50:04 +0000

    CI: Cython needs to be in build env, which is not current env. Run quick tests on built wheels

--- .ci/build_wheels.sh
+++ .ci/build_wheels.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-python -m pip install cython setuptools wheel
 python -m pip install cibuildwheel==1.7.2
 
-# ZLIB is compiled into indexed_gzip on Windows - see
-# .ci/download_zlib.sh and setup.py
+export CIBW_BEFORE_BUILD="pip install cython"
+
+# ZLIB is compiled into indexed_gzip on Windows -
+# see .ci/download_zlib.sh and setup.py
 export CIBW_ENVIRONMENT_WINDOWS="ZLIB_HOME='$ZLIB_HOME'"
+
+# Run quick test suite on built wheels
+export CIBW_TEST_REQUIRES="pytest pytest-cov coverage numpy nibabel"
+export CIBW_TEST_COMMAND="cp {project}/setup.cfg {project}/conftest.py .; pytest {project}/indexed_gzip/tests -m 'not slow_test'"
 
 python -m cibuildwheel --output-dir ./dist

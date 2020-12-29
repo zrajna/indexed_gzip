acdd762d9a7c34aeb4bf57d2c18c81bf17d562b9
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-29 19:51:59 +0000

    CI: bash expr does not support non-greedy matches - use cut instead

--- .ci/download_zlib.sh
+++ .ci/download_zlib.sh
@@ -14,8 +14,9 @@ ZLIB_HOME=$(pwd)/zlib-1.2.11
 # drive:/path/to/zlib. Only works for drives
 # with a single letter
 if [[ "$PLATFORM" == "windows"* ]]; then
-  drive=$(expr match "$p" '\(^\/[:alpha:]*\/\)')
-  ZLIB_HOME="${drive:1:1}:/${ZLIB_HOME:3}"
+  drive=$(echo "$ZLIB_HOME" | cut -d / -f 2)
+  offset=$(expr ${#drive} + 2)
+  ZLIB_HOME="${drive}:/${ZLIB_HOME:$offset}"
 fi
 
 echo "Setting ZLIB_HOME: $ZLIB_HOME"

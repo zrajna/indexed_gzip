d0bce1695f2223b77ccf3436b9aaca515c2f7467
Paul McCarthy <pauldmccarthy@gmail.com>
2020-12-29 14:20:52 +0000

    CI: A-ha, numpy is required to build the test modules

--- .ci/build_wheels.sh
+++ .ci/build_wheels.sh
@@ -3,7 +3,8 @@
 python -m pip install cibuildwheel==1.7.2
 
 # Make sure cython is available on all platforms
-export CIBW_BEFORE_BUILD="pip install cython"
+# Numpy is required to build the test modules
+export CIBW_BEFORE_BUILD="pip install cython numpy"
 
 # Make sure zlib headers are available on linux
 export CIBW_BEFORE_ALL_LINUX="yum install -y zlib-devel"
@@ -12,14 +13,11 @@ export CIBW_BEFORE_ALL_LINUX="yum install -y zlib-devel"
 # see .ci/download_zlib.sh and setup.py
 export CIBW_ENVIRONMENT_WINDOWS="ZLIB_HOME='$ZLIB_HOME'"
 
-export CIBW_BUILD_VERBOSITY=3
-
 # Run quick test suite on built wheels. We need
 # cython for the Crthon.Coverage plugin.
 export CIBW_TEST_REQUIRES="cython pytest pytest-cov coverage numpy nibabel"
 PREP="cp {project}/.coveragerc {project}/setup.cfg {project}/conftest.py ."
-DBG="tar tf {packages}; ls; python -c 'import indexed_gzip.tests as t; import os; import os.path as op; print(os.listdir(op.dirname(t.__file__)))'"
 RUN="pytest -m 'not slow_test' --pyargs indexed_gzip --import-mode=append"
-export CIBW_TEST_COMMAND="$PREP; $DBG; $RUN"
+export CIBW_TEST_COMMAND="$PREP; $RUN"
 
 python -m cibuildwheel --output-dir ./dist

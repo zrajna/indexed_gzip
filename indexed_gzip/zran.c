82d5c57324b52f72b874d9a69adf2750576c705f
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-02-27 22:22:32 -0500

    better comments

--- indexed_gzip/zran.c
+++ indexed_gzip/zran.c
@@ -49,6 +49,9 @@ static uint32_t max(uint32_t a, uint32_t b) {
 }
 #endif
 
+/*
+ * Implements a method analogous to fread that is performed on Python file-like objects.
+ */
 size_t _fread_python(void *ptr, size_t size, size_t nmemb, PyObject *f) {
     PyObject *data;
     if ((data = PyObject_CallMethod(f, "read", "(n)", size * nmemb)) == NULL) {
@@ -67,6 +70,9 @@ size_t _fread_python(void *ptr, size_t size, size_t nmemb, PyObject *f) {
 
 }
 
+/*
+ * Implements a method analogous to ftell that is performed on Python file-like objects.
+ */
 long int _ftell_python(PyObject *f) {
     PyObject *data;
     if ((data = PyObject_CallMethod(f, "tell", NULL)) == NULL) {
@@ -82,6 +88,9 @@ long int _ftell_python(PyObject *f) {
     return result;
 }
 
+/*
+ * Implements a method analogous to fseek that is performed on Python file-like objects.
+ */
 int _fseek_python(PyObject *f, long int offset, int whence) {
     PyObject *data;
     if ((data = PyObject_CallMethod(f, "seek", "(l,i)", offset, whence)) == NULL) {
@@ -92,15 +101,24 @@ int _fseek_python(PyObject *f, long int offset, int whence) {
     return 0;
 }
 
+/*
+ * Implements a method analogous to feof that is performed on Python file-like objects.
+ */
 int _feof_python(PyObject *f, int64_t size) {
     return _ftell_python(f) == size;
 }
 
+/*
+ * Implements a method analogous to ferror that is performed on Python file-like objects.
+ */
 int _ferror_python(PyObject *f) {
-    // TODO: better error handling for Python.
+    // TODO: implement better error handling for Python, using PyErr_Occurred.
     return 0;
 }
 
+/*
+ * Implements a method analogous to fflush that is performed on Python file-like objects.
+ */
 int _fflush_python(PyObject *f) {
     PyObject *data;
     if ((data = PyObject_CallMethod(f, "flush", NULL)) == NULL) {
@@ -111,6 +129,9 @@ int _fflush_python(PyObject *f) {
     return 0;
 }
 
+/*
+ * Implements a method analogous to fwrite that is performed on Python file-like objects.
+ */
 size_t _fwrite_python(const void *ptr, size_t size, size_t nmemb, PyObject *f) {
     PyObject *input;
     if ((input = PyBytes_FromStringAndSize(ptr, size * nmemb)) == NULL) {
@@ -134,6 +155,9 @@ size_t _fwrite_python(const void *ptr, size_t size, size_t nmemb, PyObject *f) {
     return (size_t) len / size;
 }
 
+/*
+ * Implements a method analogous to getc that is performed on Python file-like objects.
+ */
 int _getc_python(PyObject *f) {
     char buf [1];
     if (_fread_python(buf, 1, 1, f) == 0) {
@@ -142,36 +166,58 @@ int _getc_python(PyObject *f) {
     return (int) buf[0];
 }
 
+/*
+ * Calls ferror on fd if specified, otherwise the Python-specific method on f.
+ */
 int ferror_(FILE *fd, PyObject *f) {
-    // int one = ferror(fd);
-    int two = _ferror_python(f);
-    return two;
+    return fd ? ferror(fd): _ferror_python(f);
 }
 
+/*
+ * Calls fseek on fd if specified, otherwise the Python-specific method on f.
+ */
 int fseek_(FILE *fd, PyObject *f, long int offset, int whence) {
     return fd ? FSEEK(fd, offset, whence): _fseek_python(f, offset, whence);
 }
 
+/*
+ * Calls ftell on fd if specified, otherwise the Python-specific method on f.
+ */
 int ftell_(FILE *fd, PyObject *f) {
     return fd ? FTELL(fd): _ftell_python(f);
 }
 
+/*
+ * Calls fread on fd if specified, otherwise the Python-specific method on f.
+ */
 size_t fread_(void *ptr, size_t size, size_t nmemb, FILE *fd, PyObject *f) {
     return fd ? fread(ptr, size, nmemb, fd): _fread_python(ptr, size, nmemb, f);
 }
 
+/*
+ * Calls feof on fd if specified, otherwise the Python-specific method on f.
+ */
 int feof_(FILE *fd, PyObject *f, int64_t size) {
     return fd ? feof(fd): _feof_python(f, size);
 }
 
+/*
+ * Calls fflush on fd if specified, otherwise the Python-specific method on f.
+ */
 int fflush_(FILE *fd, PyObject *f) {
     return fd ? fflush(fd): _fflush_python(f);
 }
 
+/*
+ * Calls fwrite on fd if specified, otherwise the Python-specific method on f.
+ */
 size_t fwrite_(const void *ptr, size_t size, size_t nmemb, FILE *fd, PyObject *f) {
     return fd ? fwrite(ptr, size, nmemb, fd): _fwrite_python(ptr, size, nmemb, f);
 }
 
+/*
+ * Calls getc on fd if specified, otherwise the Python-specific method on f.
+ */
 int getc_(FILE *fd, PyObject *f) {
     return fd ? getc(fd): _getc_python(f);
 }

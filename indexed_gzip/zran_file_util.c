7801645bec85173557ee7a2da0995ebd50248dd1
Ashwin Ramaswami <aramaswamis@gmail.com>
2021-03-20 18:29:06 -0400

    Fix wheels build on 32-bit machines

--- indexed_gzip/zran_file_util.c
+++ indexed_gzip/zran_file_util.c
@@ -106,19 +106,40 @@ fail:
 int _fseek_python(PyObject *f, int64_t offset, int whence) {
 
     PyObject *data;
+    PyObject *seek_fn_name;
+    PyObject *whence_;
+    PyObject *offset_;
 
     _ZRAN_FILE_UTIL_ACQUIRE_GIL
 
-    data = PyObject_CallMethod(f, "seek", "(l,i)", offset, whence);
-    if (data == NULL)
+    // We can't use PyObject_CallMethod with multiple arguments
+    // because it causes tests with 32-bit OS wheels to fail
+    // (as the second argument is not passed correctly to Python),
+    // so we have to manually build up the arguments instead.
+    // TODO: File an issue with cpython about this.
+    // if ((data = PyObject_CallMethod(f, "seek", "(l,i)", offset, whence)) == NULL)
+    //     goto fail;
+    if ((seek_fn_name = PyUnicode_FromString("seek")) == NULL)
+        goto fail;
+    if ((whence_ = PyLong_FromLong(whence)) == NULL)
+        goto fail;
+    if ((offset_ = PyLong_FromLong(offset)) == NULL)
+        goto fail;
+    if ((data = PyObject_CallMethodObjArgs(f, seek_fn_name, offset_, whence_, NULL)) == NULL)
         goto fail;
 
     Py_DECREF(data);
+    Py_DECREF(seek_fn_name);
+    Py_DECREF(whence_);
+    Py_DECREF(offset_);
     _ZRAN_FILE_UTIL_RELEASE_GIL
     return 0;
 
 fail:
     Py_XDECREF(data);
+    Py_XDECREF(seek_fn_name);
+    Py_XDECREF(whence_);
+    Py_XDECREF(offset_);
     _ZRAN_FILE_UTIL_RELEASE_GIL
     return -1;
 }
